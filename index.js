36feb53d51888f78559c1b3007bf610c4fbf7174
kneearrow <fillion.jonathan@gmail.com>
2023-12-16 19:00:26 -0500

    test

--- index.js
+++ index.js
@@ -6,19 +6,28 @@ var beautify = require('js-beautify/js').js
 const targets = [
   "https://hotellabs.marriott.com/js/app.387a4a64.js",
   "https://hotellabs.marriott.com/js/chunk-vendors.069eb9cb.js",
-  "https://www.marriott.com/resources/1886d59e46406f938b23f5e9f0ee55b0f0aafa2feb045"
+  "https://www.marriott.com/resources/1886d59e46406f938b23f5e9f0ee55b0f0aafa2feb045",
+  "https://extranetcloud.marriott.com/common/js/marriottCommon.js?seed=AICjamuMAQAA0QfnXoQYf90iZN_cvRXhEEFC2kDo-Ua7B2gyygGiwjw3FpW2&X-aZnN0eCb--z=asdasd",
+  "https://www.marriott.com/rwuW/bALl/Dnlx/75/cJlw/9Q3mkfXhSm/ZmILAg/Dj94OXV/xGScB",
 ];
 
 async function main() {
   for (const target of targets) {
     await sleep(1000);
-    const response = await axios.get(target);
     const properFileName = target.replace(/https?:\/\//, "").replace(/\//g, "_");
-    const cleanJs = beautify(response.data, { indent_size: 2, space_in_empty_paren: true });
-    fs.writeFileSync(`responses/${properFileName}`, `${cleanJs}`, {
-      flag: "w",
-      encoding: "utf-8",
-    });
+    try {
+      const response = await axios.get(target);
+      const cleanJs = beautify(response.data, { indent_size: 2, space_in_empty_paren: true });
+      fs.writeFileSync(`responses/${properFileName}`, `${cleanJs}`, {
+        flag: "w",
+        encoding: "utf-8",
+      });
+    } catch (error) {
+      fs.writeFileSync(`responses/${properFileName}`, `${error}`, {
+        flag: "w",
+        encoding: "utf-8",
+      });
+    }
   }
 }
 
